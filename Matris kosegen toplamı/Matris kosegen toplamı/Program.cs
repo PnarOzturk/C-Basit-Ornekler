61f3fbf686dd3d921373a84a47769734318427a7
Pinar Nur Ozturk <96991858+PnarOzturk@users.noreply.github.com>
2023-09-04 21:53:22 +0300

    Add files via upload
    C# dilinde boyutu kullanici girisli, random sayilardan olusan matris kosegen toplami

---
+++ Matris kosegen toplamı/Matris kosegen toplamı/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matris_kosegen_toplamı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+             
+
+                Console.WriteLine("\n 2 boyutlu dizinin olusmasini istediniz eleman sayisini giriniz : ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[,] dizi = new int[n, n];
+                Console.WriteLine("\n\n");
+                Random rnd = new Random();
+
+                int i, j;
+                int ana = 0, asal = 0; //ana: ana köşegen, asal: asal/ters kösegen
+
+
+                for (i = 0; i < dizi.GetLength(0); i++) //Dizi satırı kadar 
+                {
+                    for (j = 0; j < dizi.GetLength(1); j++) //Dizi sütunu kadar
+                    {
+                        dizi[i, j] = rnd.Next(1, 11);
+
+                        Console.Write(dizi[i, j] + "\t");
+
+
+                        if (i == j)//ana köşegen kuralı
+                        {
+                            ana += dizi[i, j];
+                        }
+                        if (i + j == (n-1))//matris eleman sayisinin 1 eksiğini yazıyoruz.ters köşegen kuralı
+                        {
+                            asal += dizi[i, j];
+
+
+                        }
+                    }
+                    Console.WriteLine(); // Bunu yazmayınca kare matris seklinde yazilmiyor yan yana yazılıyor
+                }
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("\n\n\n");
+                Console.WriteLine("Ana köşegen Toplamı = {0}\nTers Köşegen Toplamı ={1}", ana, asal);
+                Console.WriteLine("kösegenlerindeki toplam eleman sayisi : " + (ana + asal));
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\n\n\n");
+                Console.WriteLine("Ana köşegen Toplamı = {0}\nTers Köşegen Toplamı ={1}", ana, asal);
+                Console.WriteLine("kösegenlerindeki toplam eleman sayisi : " + (ana + (asal - dizi[(n/2)+1,(n/2)+1])));
+                Console.ReadKey();
+            }
+
+
+
+
+
+        }
+    }
+ }
+
---
+++ Matris kosegen toplamı/Matris kosegen toplamı/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Matris kosegen toplamı")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Matris kosegen toplamı")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("9a52b36c-b0b1-4da0-b236-29d51b876407")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
